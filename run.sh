0503445a212b6ff9661eb0cdebaf6a9767ec552b
kh.adelya <AKhusnullina@croc.ru>
2022-07-24 22:49:50 +0300

    Dockerfile upd

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ docker run \
 	--rm \
 	-ti \
 	--mount type=bind,source="$(pwd)/input_data",target="/input_data" \
+	--mount type=bind,source="$(pwd)/output_data",target="/output_data" \
 	-e WORKER_CONFIG="/input_data/worker-config.json" \
 	"adelya/object-detection-worker" \
 	bash
