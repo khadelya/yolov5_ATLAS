c7ab8d714b83fdffd9bedbbed85131cb279b5f4c
Andrew <andrew.tar.dm@gmail.com>
2022-07-24 23:12:14 +0300

    fix entrypoint

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
+outputDir="$(pwd)/output_data"
+mkdir -p "${outputDir}"
 docker run \
 	--rm \
 	-ti \
 	--mount type=bind,source="$(pwd)/input_data",target="/input_data" \
-	--mount type=bind,source="$(pwd)/output_data",target="/output_data" \
+	--mount type=bind,source="${outputDir}",target="/output_data" \
 	-e WORKER_CONFIG="/input_data/worker-config.json" \
 	"adelya/object-detection-worker" \
 	bash
