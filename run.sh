d086625da3e8fa078f53b37f8efd98c965a167d8
Adele <102168940+khadelya@users.noreply.github.com>
2022-07-29 14:52:02 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 set -e
 set -x
 
-outputDir="$(pwd)/output_data"
+outputDir="$(pwd)/runs"
 mkdir -p "${outputDir}"
 docker run \
 	--rm \
 	-ti \
 	--ipc=host \
 	--mount type=bind,source="$(pwd)/input_data",target="/input_data" \
-	--mount type=bind,source="${outputDir}",target="/output_data" \
+	--mount type=bind,source="${outputDir}",target="/runs" \
 	-e WORKER_CONFIG="/input_data/worker-config.json" \
 	"adelya/object-detection-worker" \
 	bash
