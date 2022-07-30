8738d527610a5360564e0bf571e952bbf213ab1e
Adele <102168940+khadelya@users.noreply.github.com>
2022-07-30 17:18:31 +0300

    change runs to output_data

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 set -e
 set -x
 
-outputDir="$(pwd)/runs"
+outputDir="$(pwd)/output_data"
 mkdir -p "${outputDir}"
 docker run \
 	--rm \
 	-ti \
 	--ipc=host \
 	--mount type=bind,source="$(pwd)/input_data",target="/input_data" \
-	--mount type=bind,source="${outputDir}",target="/runs" \
+	--mount type=bind,source="${outputDir}",target="/output_data" \
 	-e WORKER_CONFIG="/input_data/worker-config.json" \
 	"adelya/object-detection-worker" \
 	bash
