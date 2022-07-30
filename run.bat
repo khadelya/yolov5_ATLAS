8b749c109a434256e52dc71219f5732c052098b1
Adele <102168940+khadelya@users.noreply.github.com>
2022-07-30 17:20:32 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,3 +0,0 @@
-docker run --rm -v C:\yolov5_docker\data:/data -e WORKER_CONFIG=/data/worker-config.json -it adelya/object-detection-worker bash
-
-docker run --rm -v C:\yolov5_docker\data:/data -v C:\yolov5_docker\annotations:/annotations -e WORKER_CONFIG=/data/worker-config.json -it adelya/object-detection-worker /bin/bash
