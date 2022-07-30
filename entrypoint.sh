41d591da7adaffcd5b66840917ce303867214d1c
Adele <102168940+khadelya@users.noreply.github.com>
2022-07-30 17:22:17 +0300

    cd runs to output_data

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,4 @@ python train.py --data /input_data/dataset.yml --weights yolov5s.pt
 python /usr/src/yolov5/annotations/tif_to_patches.py
 python detect.py --source data/temp --weights runs/train/exp/weights/best.pt --conf 0.4 --name results_detected_exp --save-txt
 python /usr/src/yolov5/annotations/results_to_json.py
+cd -r runs/ /output_data/
